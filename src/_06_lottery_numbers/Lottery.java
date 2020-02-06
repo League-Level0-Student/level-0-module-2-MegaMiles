c15f20aa4cc8dd7ed6a22b1da4367bcaff5d9841
League Student <league@iMac-29.attlocal.net>
2020-02-05 18:27:57 -0800

    animal farm

---
+++ src/_06_lottery_numbers/Lottery.java
@@ -0,0 +1,45 @@
+package _06_lottery_numbers;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Lottery {
+public static void main(String[] args) {
+Random ran = new Random();
+int number1 = ran.nextInt(10);
+int number2 = ran.nextInt(10);	
+int number3 = ran.nextInt(10);
+int number4 = ran.nextInt(10);
+int number5 = ran.nextInt(10);
+int number6 = ran.nextInt(10);
+int number7 = ran.nextInt(10);
+int number8 = ran.nextInt(10);
+int number9 = ran.nextInt(10);
+int number10= ran.nextInt(10);
+int number11= ran.nextInt(10);
+int number12= ran.nextInt(10);
+int number13= ran.nextInt(10);
+int number14= ran.nextInt(10);
+int number15= ran.nextInt(10);
+int number16= ran.nextInt(10);
+int number17= ran.nextInt(10);
+int number18= ran.nextInt(10);
+int number19= ran.nextInt(10);
+int number20= ran.nextInt(10);
+JOptionPane.showMessageDialog(null," Your Lottery Ticket Is " +number1+number2+number3+number4+number5+number6+number7+number8+number9+number10+number11+number12+number13+number14+number15+number16+number17+number18+number19+number20+ " You Lost! :(");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}}	
+
--- src/_07_animal_farm/AnimalFarm.java
+++ src/_07_animal_farm/AnimalFarm.java
@@ -16,11 +16,29 @@ public class AnimalFarm {
 	
 	public static void main(String[] args) {
 			
-		/* 1. Ask the user which animal they want, then play the sound of that animal. */			 
+		/* 1. Ask the user which animal they want, then play the sound of that animal. */
+for (int i = 0; i < 10; i++) {
+	
+		
+		
+	String animal =  JOptionPane.showInputDialog("what animal do you want");
+	if(animal.equalsIgnoreCase("cow")) {
+		playMoo();}
+		if(animal.equalsIgnoreCase("duck")) {
+			playQuack();}	
+			if(animal.equalsIgnoreCase("dog")) {
+				playWoof();}
+				if(animal.equalsIgnoreCase("cat")) {
+					playMeow();}
+					if(animal.equalsIgnoreCase("llama")) {
+						playLlama();}
+	}}
+			
+			
 			
 		/* 2. Make it so that the user can keep entering new animals. */
 
-	}
+	
 
 	static void playMoo() {
 		playNoise(mooFile);
